3b1f90e6fc192dce43194ec3c0ad02331b520196
Matthew Bryant <mandatory@gmail.com>
2015-08-18 01:03:56 -0700

    Added initial fingerprint DB

---
+++ fingerprint_db.js
@@ -0,0 +1,12 @@
+/*
+ * This is the master fingerprint database. Here users can add in fingerprints for other routers they wish to be able to identify with this framework.
+*/
+var fingerprints = [
+    {
+        'name': "ASUS RT-N66U",
+        'fingerprints': ["/images/New_ui/asustitle.png","/images/loading.gif","/images/alertImg.png","/images/New_ui/networkmap/line_one.png","/images/New_ui/networkmap/lock.png","/images/New_ui/networkmap/line_two.png","/index_style.css","/form_style.css","/NM_style.css","/other.css"],
+        'callback': function( ip ) {
+            // Insert exploit here
+        },
+    },
+]
