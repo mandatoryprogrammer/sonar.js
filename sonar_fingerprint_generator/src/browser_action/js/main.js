c856aafc6d33ec41dd91a6b238f7d6882f0c905e
mandy <mbryant@bishopfox.com>
2015-08-21 15:37:30 -0700

    Removed duplicate resource links from fingerprint list generator

--- sonar_fingerprint_generator/src/browser_action/js/main.js
+++ sonar_fingerprint_generator/src/browser_action/js/main.js
@@ -2,7 +2,7 @@ chrome.tabs.getSelected( null, function( tab ) {
   chrome.tabs.executeScript( tab.id, { file: "/src/inject/inject.js" }, function( return_data ) {
     var code = "var fingerprints = [\r\n    {\r\n        \'name\': [FINGERPRINT_NAME_REPLACE_ME],\r\n        \'fingerprints\': [FINGERPRINT_REPLACE_ME],\r\n        \'callback\': function( ip ) {\r\n            \/\/ Insert exploit here\r\n        },\r\n    },\r\n]";
     code = code.replace( '[FINGERPRINT_NAME_REPLACE_ME]', JSON.stringify( 'Example' ) );
-    code = code.replace( '[FINGERPRINT_REPLACE_ME]', JSON.stringify( return_data[0] ) );
+    code = code.replace( '[FINGERPRINT_REPLACE_ME]', JSON.stringify( remove_duplicates( return_data[0] ) ) );
     document.getElementById( 'codeoutput' ).value = code;
   });
 });
@@ -12,3 +12,15 @@ function copycode() {
   document.execCommand("copy", false, null);
 }
 document.getElementById("copy2clipboardbutton").addEventListener("click", copycode);
+
+function remove_duplicates(arr) {
+    var obj = {};
+    for (var i = 0; i < arr.length; i++) {
+        obj[arr[i]] = true;
+    }
+    arr = [];
+    for (var key in obj) {
+        arr.push(key);
+    }
+    return arr;
+}
