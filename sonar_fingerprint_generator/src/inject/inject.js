ec72407b063d6572ad2bc619a2c952cfca7071e0
Matthew Bryant <mandatory@gmail.com>
2015-08-23 23:17:53 -0700

    Improved fingerprinting to spider iframes as well

--- sonar_fingerprint_generator/src/inject/inject.js
+++ sonar_fingerprint_generator/src/inject/inject.js
@@ -7,16 +7,16 @@ function get_relative_path( url ) {
     return el.port + ':' + el.pathname;
   }
 }
-function get_resource_array() {
+function get_resource_array( document_ref ) {
   var prints = [];
-  for( var i = 0; i < document.images.length; i++ ){
-    if( document.images[i].src !== undefined ) {
-      prints.push( document.images[i].src );
+  for( var i = 0; i < document_ref.images.length; i++ ){
+    if( document_ref.images[i].src !== undefined ) {
+      prints.push( document_ref.images[i].src );
     }
   }
-  for( var i = 0; i < document.styleSheets.length; i++ ){
-    if( document.styleSheets[i].href !== undefined ) {
-      prints.push( document.styleSheets[i].href );
+  for( var i = 0; i < document_ref.styleSheets.length; i++ ){
+    if( document_ref.styleSheets[i].href !== undefined ) {
+      prints.push( document_ref.styleSheets[i].href );
     }
   }
   for( var i = 0; i < prints.length; i++ ){
@@ -24,4 +24,8 @@ function get_resource_array() {
   }
   return prints;
 }
-get_resource_array();
+var resource_array = get_resource_array( window.document );
+for( i = 0; i < window.frames.length; i++ ) {
+    resource_array = resource_array.concat( get_resource_array( window.frames[i].document ) );
+}
+resource_array
